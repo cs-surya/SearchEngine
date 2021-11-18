efc9e46b37511d715aefb1d8ba96901869d35c0b
Rahul Narayanan <rahulnarayanan@zohomail.com>
2021-11-18 14:56:55 -0500

    Changes added

---
+++ src/search/Constants.java
@@ -0,0 +1,5 @@
+package search;
+
+public class Constants {
+	public static String CRAWLER_DEFAULT_URL = "https://scholar.google.com";
+}
--- src/search/SearchEngine.java
+++ src/search/SearchEngine.java
@@ -1,5 +1,22 @@
 package search;
 
-public class SearchEngine {
+import java.util.Scanner;
 
+public class SearchEngine {
+	public static void main(String[] args) {
+		System.out.println("*******************************************");
+		System.out.println("\tWelcome to the search engine");
+		
+		System.out.println("\nEnter the URL to crawl. \nIf you like to continue with our default URL ("+Constants.CRAWLER_DEFAULT_URL+"), press 'ENTER'");
+		
+		Scanner sc = new Scanner(System.in);
+	
+		String url = sc.nextLine();
+		
+		if(url == null || url.isEmpty()) {
+			url = Constants.CRAWLER_DEFAULT_URL;
+		}
+		
+		sc.close();
+	}
 }
